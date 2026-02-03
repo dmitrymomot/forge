973e8917946b4380faa61aaa53195e91ad30f032
Dmytro Momot <mail@dmomot.com>
2026-02-03 15:38:23 +0700

    test(htmx): improve import organization in test files
    Reorganizes imports to follow stdlib/external/local convention

--- pkg/htmx/location_test.go
+++ pkg/htmx/location_test.go
@@ -6,9 +6,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/dmitrymomot/forge/pkg/htmx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/dmitrymomot/forge/pkg/htmx"
 )
 
 func TestLocation(t *testing.T) {
