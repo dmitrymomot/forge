395a2175b6f76f04e962c6d72b93520e8043862a
Dmytro Momot <mail@dmomot.com>
2026-06-26 21:10:17 +0700

    feat(render): add Components multi-fragment writer

--- render/errors.go
+++ render/errors.go
@@ -8,5 +8,8 @@ var ErrNilTemplate = errors.New("render: nil template")
 // ErrNilComponent is returned by Templ when the provided component is nil.
 var ErrNilComponent = errors.New("render: nil component")
 
+// ErrNoComponents is returned by Components when no components are provided.
+var ErrNoComponents = errors.New("render: no components")
+
 // ErrNilBody is returned by Stream and Attachment when the body reader is nil.
 var ErrNilBody = errors.New("render: nil body")
