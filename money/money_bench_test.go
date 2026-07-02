6e07482549151c1c673379b02e902f7087699381
Dmytro Momot <mail@dmomot.com>
2026-07-02 12:02:42 +0700

    test(money): add benchmarks

---
+++ money/money_bench_test.go
@@ -0,0 +1,121 @@
+package money_test
+
+import (
+	"testing"
+
+	"github.com/dmitrymomot/forge/decimal"
+	"github.com/dmitrymomot/forge/money"
+)
+
+func BenchmarkNew(b *testing.B) {
+	amount := decimal.New(150, 2) // 1.50
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = money.New(amount, money.USD)
+	}
+}
+
+func BenchmarkFromMinor(b *testing.B) {
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = money.FromMinor(150, money.USD)
+	}
+}
+
+func BenchmarkParse(b *testing.B) {
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = money.Parse("1234.56", money.USD)
+	}
+}
+
+func BenchmarkAmount(b *testing.B) {
+	m := money.FromMinor(150, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = m.Amount()
+	}
+}
+
+func BenchmarkMinor(b *testing.B) {
+	m := money.FromMinor(150, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = m.Minor()
+	}
+}
+
+func BenchmarkString(b *testing.B) {
+	m := money.FromMinor(150, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = m.String()
+	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	m := money.FromMinor(150, money.USD)
+	n := money.FromMinor(275, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = m.Add(n)
+	}
+}
+
+func BenchmarkSub(b *testing.B) {
+	m := money.FromMinor(500, money.USD)
+	n := money.FromMinor(275, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = m.Sub(n)
+	}
+}
+
+func BenchmarkMul(b *testing.B) {
+	m := money.FromMinor(1000, money.USD)
+	factor := decimal.New(825, 3) // 0.825 tax-like rate
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = m.Mul(factor)
+	}
+}
+
+func BenchmarkRound(b *testing.B) {
+	m, _ := money.Parse("1.23456", money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_ = m.Round(decimal.HalfEven)
+	}
+}
+
+func BenchmarkCmp(b *testing.B) {
+	m := money.FromMinor(150, money.USD)
+	n := money.FromMinor(275, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = m.Cmp(n)
+	}
+}
+
+func BenchmarkAllocate(b *testing.B) {
+	m := money.FromMinor(1000, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = m.Allocate(1, 2, 3)
+	}
+}
+
+func BenchmarkSplit(b *testing.B) {
+	m := money.FromMinor(1000, money.USD)
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = m.Split(3)
+	}
+}
+
+func BenchmarkCurrencyByCode(b *testing.B) {
+	b.ReportAllocs()
+	for b.Loop() {
+		_, _ = money.CurrencyByCode("usd")
+	}
+}
